52c83db83e08f4955ef90d091f8e42d106e0dbe1
Danang Priambodo <mdanangpriambodo@gmail.com>
2024-05-17 17:14:34 +0700

    add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 // import { MainNav } from "./main-nav";
 // import UserButton from "./user-button";
 
+import Image from "next/image";
 import { HamburgMenu } from "./HamburgMenu";
 import { ModeToggle } from "./ModeToggle";
 import CustomLink from "./custom-link";
@@ -13,7 +14,31 @@ export default function Header() {
         {/* <MainNav />
         <UserButton /> */}
         <div className="flex">
-          <ModeToggle />
+          <a className="flex gap-2 items-center" href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-bird dark:text-white"
+            >
+              <path d="M16 7h.01" />
+              <path d="M3.4 18H12a8 8 0 0 0 8-8V7a4 4 0 0 0-7.28-2.3L2 20" />
+              <path d="m20 7 2 .5-2 .5" />
+              <path d="M10 18v3" />
+              <path d="M14 17.75V21" />
+              <path d="M7 18a6 6 0 0 0 3.84-10.61" />
+            </svg>
+
+            <h1 className="text-lg font-bold leading-tight tracking-tighter lg:leading-[1.1]">
+              Boilerplate
+            </h1>
+          </a>
         </div>
         <div>
           <div className="hidden sm:flex gap-2">
@@ -29,8 +54,8 @@ export default function Header() {
             <ModeToggle />
           </div>
           <div className="flex sm:hidden gap-2">
-            <HamburgMenu />
             <ModeToggle />
+            <HamburgMenu />
           </div>
         </div>
       </div>
